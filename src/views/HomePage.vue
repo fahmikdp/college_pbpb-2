e15f59f8536ac3eb52f32602e6ba7b61f54e7250
Fahmi <fahmikurnia04@gmail.com>
2026-06-24 11:47:19 +0700

    first

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,27 +2,112 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Cuaca Jakarta</ion-title>
       </ion-toolbar>
     </ion-header>
 
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
+          <ion-title size="large">Cuaca Jakarta</ion-title>
         </ion-toolbar>
       </ion-header>
-
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <div v-if="loading">
+          <p>Memuat data...</p>
+        </div>
+        <div v-else-if="error">
+          <p>Gagal memuat data cuaca : {{ error.message }}</p>
+        </div>
+        <ion-list v-else lines="full">
+            <ion-item v-for="(item, index) in forecast" :key="index">
+              <ion-label>
+                Jam: {{ item.time }}
+                <p>Suhu: {{ item.temperature }} {{ item.temperature_type }}</p>
+              </ion-label>
+            </ion-item>
+        </ion-list>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonLabel } from '@ionic/vue';
+import { ref, onMounted } from 'vue';
+
+interface ForecastItem{
+  time: string;
+  temperature: number;
+  temperature_type: string;
+}
+
+interface weatherApiResponse {
+  hourly_units: {
+    temperature_2m: string;
+  },
+  hourly:{
+    time: string[];
+    temperature_2m: number[];
+  }
+}
+
+const forecast = ref<ForecastItem[]>([]);
+const loading = ref(true);
+const error = ref<Error | null>(null);
+
+const apiWeather = "https://api.open-meteo.com/v1/forecast?latitude=-6.2&longitude=106.8&hourly=temperature_2m";
+
+function formatHour(timestamp: string): string{
+  const date = new Date(timestamp);
+
+  return date.toLocaleTimeString('id-ID', {
+    hour: '2-digit',
+    minute: '2-digit',
+    timeZone: 'Asia/Jakarta'
+  });
+}
+
+async function fetchWeather() {
+  
+  try{
+
+    const response = await fetch(apiWeather);
+
+    if(!response.ok){
+      throw new Error(`HTTP Error! status : ${response.status}`);
+    }
+
+    const data: weatherApiResponse = await response.json();
+    const processedData = data.hourly.time.map((time, index) => {
+      return {
+        time: formatHour(time),
+        temperature: data.hourly.temperature_2m[index],
+        temperature_type: data.hourly_units.temperature_2m
+      }
+    }).slice(0,24);
+    forecast.value = processedData;
+    
+
+  }catch(e){
+    
+    if(e instanceof Error){
+      error.value = e;
+    }else{
+      error.value = new Error("Terjadi Kesalahan")
+    }
+
+  }finally{
+    
+    loading.value = false;
+
+  }  
+}
+
+onMounted(() => {
+  fetchWeather();
+})
+
 </script>
 
 <style scoped>
